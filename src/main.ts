4cc195b6d5d05fe63bf4da56a5dd3e349f7a8a84
ddcgithub <ddcd276@gmail.com>
2023-11-02 08:39:49 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,24 @@ import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
